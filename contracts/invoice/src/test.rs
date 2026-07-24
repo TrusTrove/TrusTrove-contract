ef17c549b332bf7d37d190b6ce0f0897a1533e87
muhsar27 <muhsar4321@gmail.com>
2026-07-24 23:04:27 +0100

    test: add unit tests for invoice contract lifecycle and state transitions

--- contracts/invoice/src/test.rs
+++ contracts/invoice/src/test.rs
@@ -872,7 +872,9 @@ fn test_create_fails_counter_overflow() {
     let due_date = env.ledger().timestamp() + 86400;
 
     env.as_contract(&client.address, || {
-        env.storage().instance().set(&crate::DataKey::Counter, &u64::MAX);
+        env.storage()
+            .instance()
+            .set(&crate::DataKey::Counter, &u64::MAX);
     });
 
     client.create(&issuer, &buyer, &1_000_000_000, &due_date, &usdc);
